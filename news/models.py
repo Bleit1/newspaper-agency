8a8488004af2d3babe5356dad7dd7dbe0ed75a79
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-02-03 10:24:30 +0200

    Add models: Topic and NewspaperIssue with relationships

--- news/models.py
+++ news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Topic(models.Model):
+    name = models.CharField("Name Topics", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class NewspaperIssue(models.Model):
+    title = models.CharField("Title of the news", max_length=200)
+    content = models.TextField("Content of news")
+    publication_date = models.DateField("Public date", auto_now_add=True)
+    editors = models.ManyToManyField(User, verbose_name="Editors")
+    topics = models.ManyToManyField(Topic, verbose_name="Topics", blank=True)
+
+    def __str__(self):
+        return self.title
