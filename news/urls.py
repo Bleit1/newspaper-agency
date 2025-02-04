0c014da0cfb180af1ef6cb0f736e8d8ee9690631
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-02-04 15:08:41 +0200

    fix problem with authorization

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.issue_list, name='issue_list'),
     path('main/', views.main, name='main'),
@@ -8,8 +9,9 @@ urlpatterns = [
     path('news/<int:pk>/comment/', views.add_comment, name='add_comment'),
     path('news/<int:pk>/react/', views.add_reaction, name='add_reaction'),
     path('create_news/', views.create_news, name='create_news'),
-    path('login/', views.custom_login, name='login'),
     path('latest/', views.latest, name='latest'),
-    path('register/', views.register, name='register'),
+    path('login/', views.custom_login, name='custom-login'),
     path('logout/', views.custom_logout, name='logout'),
+
+    path('register/', views.register, name='register'),
 ]
