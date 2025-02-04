d86927de2d8ca9ecda6c3d99c37cbeb577bd9ae9
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-02-04 10:19:50 +0200

    navigation

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.issue_list, name='issue_list'),
-    path('issue/<int:pk>/', views.issue_detail, name='issue_detail'),
+    path('main/', views.main, name='main'),
+    path('latest/', views.latest, name='latest'),
+    path('news/<int:pk>/', views.issue_detail, name='issue_detail'),
+    path('news/<int:pk>/comment/', views.add_comment, name='add_comment'),
+    path('news/<int:pk>/react/', views.add_reaction, name='add_reaction'),
+    path('create_news/', views.create_news, name='create_news'),
+    path('login/', views.custom_login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.custom_logout, name='logout'),
 ]
