8866972ee52ffc841971708e0160a842d156d13f
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-02-05 14:15:36 +0200

    fix problems with groups

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Topic, NewspaperIssue
+from .models import NewspaperIssue, Topic
 
+class NewspaperIssueAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publication_date', 'featured')
+    list_filter = ('featured', 'publication_date', 'topics')
+    search_fields = ('title', 'content')
+    filter_horizontal = ('editors', 'topics')
+
+admin.site.register(NewspaperIssue, NewspaperIssueAdmin)
 admin.site.register(Topic)
-admin.site.register(NewspaperIssue)
